047a7ee17b8af3cc9be70ac71367d4f8b411c861
cngo <Hieu_Ngo@intuit.com>
2019-04-20 15:32:07 -0600

    Added anonymous auth

--- App.js
+++ App.js
@@ -6,24 +6,65 @@ import firebase from 'react-native-firebase';
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      isAuthenticated: false
+    };
   }
 
   async componentDidMount() {
     // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
+     const { user } = await firebase.auth().signInAnonymously();
+     this.setState({
+      isAuthenticated: true
+     })
+     console.log('User -> ', user.toJSON());
 
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+     await firebase.analytics().logEvent('foo', { bar: '123'});
+  }
+
+  getFirebaseModulesView() {
+    return (
+      <View style={styles.modules}>
+          <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
+          {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
+          {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
+          {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
+          {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
+          {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
+          {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
+          {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
+          {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
+          {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
+          {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
+          {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
+          {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
+          {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
+          {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
+          {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
+        </View>
+    );
+  }
+
+  getUserView() {
+    const user = firebase.auth().currentUser;
+    if (user == null) {
+      return null;
+    }
+    return (
+      <View>
+        <Text>{JSON.stringify(user.toJSON())}</Text>
+      </View>
+    )
   }
 
   render() {
+    const userView = this.getUserView();
     return (
       <ScrollView>
         <View style={styles.container}>
           <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
           <Text style={styles.welcome}>
-            Welcome to {'\n'} Zoo Team Front End Android App
+            Welcome to {'\n'} Beauty Product Lookup App
           </Text>
           <Text style={styles.instructions}>
             To get started, edit App.js
@@ -39,24 +80,7 @@ export default class App extends React.Component {
               Cmd+M or shake for dev menu
             </Text>
           )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
+          {userView}
         </View>
       </ScrollView>
     );
